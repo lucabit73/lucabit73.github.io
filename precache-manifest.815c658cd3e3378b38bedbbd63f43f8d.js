7ce431d6336b716358a2196a47784b36e9871c21
Luca Querci <lucabit73@gmail.com>
2020-03-26 23:14:31 +0100

    react init

---
+++ precache-manifest.815c658cd3e3378b38bedbbd63f43f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00a76d36e9fe00c8b898726b35b47c4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20585cb3afabc5af6f62",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6bd08d5e359d978f7f4",
+    "url": "/static/js/2.26e27756.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26e27756.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20585cb3afabc5af6f62",
+    "url": "/static/js/main.ae813e3e.chunk.js"
+  },
+  {
+    "revision": "b2af832ba7f613a64f71",
+    "url": "/static/js/runtime-main.8a64ad31.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
